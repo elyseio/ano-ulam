8b80b940dd92e53fe87bbe3ab3364777618678d8
elyseio <elyseio@proton.me>
2025-05-20 05:59:42 +0800

    added a function to parse the html using bs4

--- scrape.py
+++ scrape.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 # url = 'https://panlasangpinoy.com/recipes/page/1/'
 
 def get_request(url):
+    '''
+        Make request to the url
+    '''
     s = requests.Session()
     try:
         resp = s.get(url, impersonate='chrome', timeout=5)
@@ -16,7 +19,29 @@ def get_request(url):
         return
     return resp.text
 
+def parse_data(url):
+    '''
+        Parses the html content
+    '''
+    html_content = get_request(url)
+
+    if not html_content:
+        return
+
+    soup = BeautifulSoup(html_content, 'lxml')
+    main_content = soup.select_one('main.content.entries-container')
+    recipes = main_content.select('article[class*="recipes"]')
+
+    for recipe in recipes:
+        recipe_title = recipe.select_one('.entry-title').get_text()
+        recipe_link = recipe.select_one('.entry-title-link').get('href')
+        recipe_img = recipe.select_one('.entry-image').get('data-src')
+
+
+
 def main():
+    url = 'https://panlasangpinoy.com/recipes/page/1/'
+    parse_data(url)
 
 
 if __name__ == '__main__':
