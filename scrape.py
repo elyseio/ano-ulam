4c050d3e4b2348742c1497e91b4f0ae8cbc1aca2
elyseio <elyseio@proton.me>
2025-05-20 08:12:02 +0800

    finished scraping bot, then saved the recipes in recipes.json

--- scrape.py
+++ scrape.py
@@ -1,14 +1,14 @@
 from curl_cffi import requests
 from bs4 import BeautifulSoup
+import json
 
 
 def get_request(url):
     '''
         Make request to the url
     '''
-    s = requests.Session()
     try:
-        resp = s.get(url, impersonate='chrome', timeout=5)
+        resp = requests.get(url, impersonate='chrome', timeout=5)
         resp.raise_for_status()
     except requests.exceptions.Timeout:
         print('ERROR: Request timed out. Check your internet connection or try again later.')
@@ -18,9 +18,9 @@ def get_request(url):
         return
     return resp.text
 
-def parse_data(url):
+def parse_data(all_recipes, url):
     '''
-        Parses the html content
+        Parses the html content and append each recipe into all_recipes
     '''
     html_content = get_request(url)
 
@@ -31,24 +31,58 @@ def parse_data(url):
     main_content = soup.select_one('main.content.entries-container')
     recipes = main_content.select('article[class*="recipes"]')
 
-    all_recipes = []
+    if not recipes:
+        return
+
+    i = len(all_recipes)
 
     for recipe in recipes:
         recipe_name = recipe.select_one('.entry-title').get_text()
         recipe_link = recipe.select_one('.entry-title-link').get('href')
-        recipe_img = recipe.select_one('.entry-image').get('data-src')
+        recipe_img = recipe.select_one('.entry-image')
+        if recipe_img:
+            recipe_img = recipe_img.get('data-src')
+        else:
+            recipe_img = None
 
         all_recipes.append({
+            'id': i,
             'recipe_name': recipe_name or None,
             'recipe_link': recipe_link or None,
             'recipe_img': recipe_img or None
         })
+        i += 1
 
-    return all_recipes
+def save_as_json(all_recipes, recipes_file_name):
+    '''
+        Save recipes into json
+    '''
+
+    with open(recipes_file_name, 'w', encoding='utf-8') as f:
+        json.dump(all_recipes, f, ensure_ascii=False, indent=4)
 
 
 def main():
-    url = 'https://panlasangpinoy.com/recipes/page/1/'
+    page_num = 1
+    all_recipes = []
+    recipes_file_name = 'recipes.json'
+
+    try:
+        while page_num < 227:
+            url = f'https://panlasangpinoy.com/recipes/page/{page_num}'
+            print(f'Scraping page: {url}')
+            parse_data(all_recipes, url)
+            page_num += 1
+
+    except KeyboardInterrupt:
+        print('\nScraping manually interrupted. Saving data...')
+
+    print('=' * 100)
+    print('Finish scraping')
+    print('=' * 100)
+    print(f'Total recipes scraped: {len(all_recipes)}')
+    save_as_json(all_recipes, recipes_file_name)
+    print(f'Saved all recipes in: {recipes_file_name}')
 
 
 if __name__ == '__main__':
